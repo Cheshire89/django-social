179ac6b1e4e71c1cd09ce06031f5e61b38377e15
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-11-20 14:56:35 -0700

    init

---
+++ account/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . import views
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
+
+urlpatterns = [
+    path('login/', view=LoginView.as_view(), name='login'),
+    path('logout/', view=LogoutView.as_view(), name='logout'),
+
+
+    path('password-change/', view=PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', view=PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', view=PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done', view=PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('password-reset/<uidb64>/<token>/', view=PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('password-reset/complete/', view=PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+
+    path('', views.dashboard, name='dashboard'),
+]

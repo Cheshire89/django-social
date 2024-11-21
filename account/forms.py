b4a070f721a148790c6df2e0cf8390ad3b20d610
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-11-21 11:18:36 -0700

    resiter and login views

--- account/forms.py
+++ account/forms.py
@@ -1,7 +1,30 @@
 from django import forms
-
+from django.contrib.auth import get_user_model
 
 class LoginForm(forms.Form):
-    '''User login form.'''
+    '''User login form controller.'''
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
+
+
+class UserRegistrationForm(forms.ModelForm):
+    '''User registration form controller.'''
+    class Meta:
+        model = get_user_model()
+        fields = ['username', 'first_name', 'last_name', 'email']
+
+
+    password = forms.CharField(
+        label='Password',
+        widget=forms.PasswordInput
+    )
+    password2 = forms.CharField(
+        label="Confirm password",
+        widget=forms.PasswordInput
+    )
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if(cd['password'] != cd['password2']):
+            raise forms.ValidationError("Passwords do not match.")
+        return cd['password2']

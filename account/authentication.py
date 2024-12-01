f893cc43d1107f95011ba047cd219c30f40f463a
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-12-01 10:50:04 -0700

    allow login using email

--- account/authentication.py
+++ account/authentication.py
@@ -5,5 +5,16 @@ class EmailAuthBackend:
     '''Authenticate using an e-mail address.'''
 
     def authenticate(self, request, username=None, password=None):
-        '''Hey'''
-        return self
+        try:
+            user = User.objects.get(email=username)
+            if user.check_password(password):
+                return user
+            return None
+        except (User.DoesNotExist, User.MultipleObjectsReturned):
+            return None
+
+    def get_user(self, user_id):
+        try:
+            return User.objects.get(pk=user_id)
+        except (User.DoesNotExist):
+            return None

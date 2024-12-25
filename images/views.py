96fabf889e0c1384ced324922fb0f19306ae5315
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2024-12-25 14:36:48 -0700

    sharing content

--- images/views.py
+++ images/views.py
@@ -1,13 +1,56 @@
+from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render, get_object_or_404
 from .forms import ImageCrateForm
 from .models import Image
-from django.http import HttpRequest
+from django.http import HttpRequest, HttpResponse
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 
 @login_required
-def images_like
+def image_list(request: HttpRequest):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(
+            request,
+            'images/image/list_images.html',
+            {'section': 'images', 'images': images}
+        )
+    return render(
+        request,
+        'images/image/list.html',
+        {'section': 'images', 'images': images}
+    )
+
+
+@login_required
+def image_like(request: HttpRequest):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
 
 @login_required
 def image_create(request: HttpRequest):

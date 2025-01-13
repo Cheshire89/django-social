6eb49dd64d487da9c886d478bb3675ffbf59eff4
Aleksandr Antonov <antonov.a.aleksandr@gmail.com>
2025-01-12 22:48:52 -0700

    add redis to store image ranking

--- scrapbook/settings.py
+++ scrapbook/settings.py
@@ -183,3 +183,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 ABSOLUTE_URL_OVERRIDES = {
     'auth.user': lambda u:reverse_lazy('user_detail', args=[u.username])
 }
+
+
+REDIS_HOST = 'localhost'
+REDIS_PORT = 6379
+REDIS_DB = 0
